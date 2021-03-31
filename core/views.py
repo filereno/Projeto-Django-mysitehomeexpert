2a8731674a388f0379eed6c356880c03b1f6754a
fabiano <f_filereno@hotmail.com>
2021-03-31 01:24:29 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,34 @@
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from .models import Servico, Funcionario
+from .forms import ContatoForm
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.utils import translation
+# Create your views here.
+
+
+class IndexView(FormView):
+    template_name = "index.html"
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        lang = translation.get_language()
+        #context['servicos'] = Servico.objects.all()
+        context['servicos'] = Servico.objects.order_by('?').all()
+        #context['funcionario'] = Funcionario.objects.all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        context['lang']=lang
+        translation.activate(lang)
+        return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, _('E-mail enviado com sucesso'))
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, _('Erro ao enviar'))
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
